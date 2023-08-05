0a11235d32860c033f8de7f4f70e18b3c9622b85
Potassium <brainiacgamer444@gmail.com>
2023-08-06 00:03:20 +0545

    Formatted code for better reading

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+const options = ["rock", "paper", "scissor"];
 let playerSelection;
+let computerSelection;
 let totalTries = 0;
 
 function playerInput() {
-    playerSelection = prompt("Enter your weapon: ");
-    console.log(playerSelection);
+    playerSelection = prompt("Enter your weapon: ").toLowerCase();
+    return playerSelection;
 }
 
 function computerInput() {
-    const options = ["rock", "paper", "scissor"];
-    let computerSelection = options[Math.floor(Math.random() * 3)];
-    console.log(computerSelection);
+    computerSelection = options[Math.floor(Math.random() * 3)];
     return computerSelection;
 }
 
