9e5393c1f7a7f9b9eca6ee492fec95ac4169000c
Potassium <brainiacgamer444@gmail.com>
2023-08-06 00:33:33 +0545

    Make function runGame which returns output based on the choices selected
    Made a function runGame which returns output of string based on userInput and getComputerSelection by using switch statement

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ const options = ["rock", "paper", "scissor"];
 let playerSelection;
 let computerSelection;
 let totalTries = 0;
+let output;
 
-function playerInput() {
+function userInput() {
     playerSelection = prompt("Enter your weapon: ").toLowerCase();
     return playerSelection;
 }
 
-function computerInput() {
+function getComputerSelection() {
     computerSelection = options[Math.floor(Math.random() * 3)];
     return computerSelection;
 }
 
-while (totalTries < 5) {
-    totalTries++;
-    computerInput();
-    playerInput();
+function runGame(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection === computerSelection:
+            output = `Tie!, both choose ${playerSelection}`;
+            break;
+        case playerSelection === "rock" && computerSelection === "scissor":
+        case playerSelection === "paper" && computerSelection === "rock":
+        case playerSelection === "scissor" && computerSelection === "paper":
+            output = `You win!, ${playerSelection} beats ${computerSelection}`;
+            break;
+        case playerSelection === "rock" && computerSelection === "paper":
+        case playerSelection === "paper" && computerSelection === "scissor":
+        case playerSelection === "scissor" && computerSelection === "rock":
+            output = `You lose!, ${computerSelection} beats ${playerSelection}`;
+            break;
+        default:
+            output = "Please enter a valid choice (Rock, paper or scissor)";
+            break;
+    }
+    return output;
 }
 
+while (totalTries < 5) {
+    totalTries++;
+    console.log(runGame(userInput(), getComputerSelection()));
+}
