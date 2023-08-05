85ff121d367914e36234d28cce12f168f2fa0315
Potassium <brainiacgamer444@gmail.com>
2023-08-05 23:58:59 +0545

    Make a function computerInput
    Made a function computer input to store computer option which is generated randomly

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let playerSelection;
 let totalTries = 0;
 
-function playerInput(){
+function playerInput() {
     playerSelection = prompt("Enter your weapon: ");
     console.log(playerSelection);
 }
 
-while(totalTries<5){
+function computerInput() {
+    const options = ["rock", "paper", "scissor"];
+    let computerSelection = options[Math.floor(Math.random() * 3)];
+    console.log(computerSelection);
+    return computerSelection;
+}
+
+while (totalTries < 5) {
     totalTries++;
+    computerInput();
     playerInput();
 }
+
