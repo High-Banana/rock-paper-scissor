7278b1d12ead2484a92a0ec68a2376ba3f429108
Potassium <brainiacgamer444@gmail.com>
2023-08-08 00:34:01 +0545

    Make score display in DOM

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.querySelectorAll(".btn");
 const playerInputField = document.getElementById("playerChoice");
 const computerInputField = document.getElementById("computerChoice");
 const gameResultField = document.getElementById("gameResult");
+const userScoreField = document.getElementById("user_score");
+const computerScoreField = document.getElementById("computer_score");
 
 const options = ["rock", "paper", "scissor"];
 let playerSelection;
@@ -15,14 +17,16 @@ let computerScore = 0;
 button.forEach((element) => {
     element.addEventListener("click", () => {
         playerSelection = (element.textContent).toLowerCase();
-        
+
         //To get computer's selection from options array
         computerSelection = options[Math.floor(Math.random() * 3)];
 
         function displayGameOutput(){
-            playerInputField.textContent = `You choose ${playerSelection}`;
-            computerInputField.textContent = `Computer choose ${computerSelection}`;
-            gameResultField.textContent = runGame(playerSelection, computerSelection);
+            playerInputField.textContent = `You choose ${playerSelection}`; //Show player selection
+            computerInputField.textContent = `Computer choose ${computerSelection}`; //Show CPU selection
+            gameResultField.textContent = runGame(playerSelection, computerSelection); //Show result
+            userScoreField.textContent = `Your score: ${playerScore}`; //Show player score
+            computerScoreField.textContent = `Computer score: ${computerScore}`; // Show computer score
         }
         displayGameOutput();
     })
@@ -32,7 +36,7 @@ button.forEach((element) => {
 function runGame(playerSelection, computerSelection) {
     switch (true) {
         case playerSelection === computerSelection:
-            output = `Tie!, both choose ${playerSelection}`;
+            output = `Tie! both choose ${playerSelection}`;
             break;
         case playerSelection === "rock" && computerSelection === "scissor":
         case playerSelection === "paper" && computerSelection === "rock":
